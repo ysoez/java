1cd308ef09ce6a88a2f2af5b1c11051698bb1679
Dmitry <dmitry@blockearner.com.au>
2023-02-27 18:26:13 +0200

    [ds-alg] reverse linked list

--- data/ds-alg/src/main/java/algorithm/reverse/LinkedListReverser.java
+++ data/ds-alg/src/main/java/algorithm/reverse/LinkedListReverser.java
@@ -1,20 +1,13 @@
-package algorithm;
+package algorithm.reverse;
 
 import data_structure.linked_list.UnaryNode;
 import util.Complexity;
 
-public class Reverser {
+public class LinkedListReverser<E> implements Reverser<UnaryNode<E>> {
 
-    public static int[] reverse(int[] arr) {
-        var reversedArr = new int[arr.length];
-        for (int i = arr.length - 1, j = 0; i >= 0; i--, j++) {
-            reversedArr[j] = arr[i];
-        }
-        return reversedArr;
-    }
-
+    @Override
     @Complexity(runtime = "O(n)", space = "O(1)")
-    public static <E> UnaryNode<E> reverse(UnaryNode<E> node) {
+    public UnaryNode<E> reverse(UnaryNode<E> node) {
         if (node == null)
             throw new IllegalArgumentException("Head is null");
         if (node.next() == null)
