5b43cf7c86a45ee1ccad3c5247f3c05ef3c7b46d
Dmitry <dmitry@blockearner.com.au>
2023-05-15 11:39:46 +0300

    [ds-alg] tree lca

--- data/ds-alg/src/main/java/algorithm/Trees.java
+++ data/ds-alg/src/main/java/algorithm/Trees.java
@@ -4,8 +4,11 @@ import util.Algorithm;
 import util.Algorithm.Complexity;
 import util.BinaryNode;
 
+import java.util.Stack;
+
 import static util.Algorithm.Complexity.Value.CONSTANT;
 import static util.Algorithm.Complexity.Value.LINEAR;
+import static util.Algorithm.Complexity.Value.LOGARITHMIC;
 import static util.Algorithm.Implementation.RECURSIVE;
 
 class Trees {
@@ -32,6 +35,48 @@ class Trees {
             isRightBST = isBst(node.right, node.value, upperLim);
         }
         return isLeftBST && isRightBST;
+    }
+
+    @Algorithm(complexity = @Complexity(runtime = LINEAR, space = LOGARITHMIC))
+    static Integer lca(BinaryNode<Integer> root, int j, int k) {
+        var pathToJ = pathToX(root, j);
+        var pathToK = pathToX(root, k);
+        if (pathToJ == null || pathToK == null) {
+            return null;
+        }
+        BinaryNode<Integer> lca = null;
+        while (!pathToJ.isEmpty() && !pathToK.isEmpty()) {
+            var jPop = pathToJ.pop();
+            var kPop = pathToK.pop();
+            if (jPop == kPop) {
+                lca = jPop;
+            } else {
+                break;
+            }
+        }
+        return lca != null ? lca.value : null;
+    }
+
+    private static Stack<BinaryNode<Integer>> pathToX(BinaryNode<Integer> node, int x) {
+        if (node == null) {
+            return null;
+        }
+        if (node.value == x) {
+            var path = new Stack<BinaryNode<Integer>>();
+            path.push(node);
+            return path;
+        }
+        var leftPath = pathToX(node.left, x);
+        if (leftPath != null) {
+            leftPath.push(node);
+            return leftPath;
+        }
+        var rightPath = pathToX(node.right, x);
+        if (rightPath != null) {
+            rightPath.push(node);
+            return rightPath;
+        }
+        return null;
     }
 
 }
--- data/ds-alg/src/test/java/algorithm/TreesTest.java
+++ data/ds-alg/src/test/java/algorithm/TreesTest.java
@@ -68,20 +68,58 @@ class TreesTest {
         mapping4.put(1, childrenK);
         var head4 = createTree(mapping4, 3);
         assertFalse(Trees.isBst(head4));
-
-
-
-
-
-        // isBST(head2) should return false
-        // isBST(head3) should return true
-        // isBST(head4) should return false
     }
 
+    @Test
+    void lca() {
+        assertNull(Trees.lca(null, 3, 0));
 
-    // mapping: a node-to-node mapping that shows how the tree should be constructed
+        // head1 = 0
+        //        / \
+        //       1   2
+        //      /\   /\
+        //     3  4 5  6
+        HashMap<Integer, int[]> mapping1 = new HashMap<>();
+        int[] childrenA = {1, 2};
+        int[] childrenB = {3, 4};
+        int[] childrenC = {5, 6};
+        mapping1.put(0, childrenA);
+        mapping1.put(1, childrenB);
+        mapping1.put(2, childrenC);
+        var head1 = createTree(mapping1, 0);
+
+        assertEquals(0, Trees.lca(head1, 1, 5));
+        assertEquals(1, Trees.lca(head1, 3, 1));
+        assertEquals(1, Trees.lca(head1, 1, 4));
+        assertEquals(0, Trees.lca(head1, 0, 5));
+
+        //  head2 = 5
+        //        /   \
+        //       1     4
+        //      /\    / \
+        //     3  8  9  2
+        //    /\
+        //   6  7
+        HashMap<Integer, int[]> mapping2 = new HashMap<>();
+        int[] childrenD = {1, 4};
+        int[] childrenE = {3, 8};
+        int[] childrenF = {9, 2};
+        int[] childrenG = {6, 7};
+        mapping2.put(5, childrenD);
+        mapping2.put(1, childrenE);
+        mapping2.put(4, childrenF);
+        mapping2.put(3, childrenG);
+        var head2 = createTree(mapping2, 5);
+
+        assertEquals(5, Trees.lca(head2, 4, 7));
+        assertEquals(3, Trees.lca(head2, 3, 3));
+        assertEquals(1, Trees.lca(head2, 8, 7));
+        assertNull(Trees.lca(head2, 3, 0));
+        assertNull(Trees.lca(null, 3, 0));
+    }
+
     // example: {0: [1, 2]} means that 0's left child is 1, and its right child is 2
-    public static BinaryNode<Integer> createTree(HashMap<Integer, int[]> mapping, int headValue) {
+    private static BinaryNode<Integer> createTree(HashMap<Integer, int[]> mapping, int headValue) {
         var head = new BinaryNode<>(headValue, null, null);
         var nodes = new HashMap<Integer, BinaryNode<Integer>>();
         nodes.put(headValue, head);
