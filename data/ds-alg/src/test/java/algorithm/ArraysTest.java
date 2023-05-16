85852e5bbe039da54cdb5284d9d481f780e06a26
Dmitry <dmitry@blockearner.com.au>
2023-05-16 18:37:52 +0300

    [ds-alg] refactoring

--- data/ds-alg/src/test/java/algorithm/ArraysTest.java
+++ data/ds-alg/src/test/java/algorithm/ArraysTest.java
@@ -9,10 +9,25 @@ import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class ArraysTest {
 
+    @Test
+    void max() {
+        assertThrows(IllegalArgumentException.class, () -> Arrays.max(null));
+        assertThrows(IllegalArgumentException.class, () -> Arrays.max(new int[0]));
+
+        assertEquals(1, Arrays.max(new int[]{1}));
+
+        assertEquals(2, Arrays.max(new int[]{1, 2}));
+        assertEquals(2, Arrays.max(new int[]{2, 1}));
+
+        assertEquals(78, Arrays.max(new int[]{10, 21, 16, 78, 62, 5}));
+        assertEquals(30, Arrays.max(new int[]{10, 20, 30}));
+    }
+    
     @ParameterizedTest
     @MethodSource("twoSumDataset")
     void twoSum(int[] nums, int target, int[] result) {
