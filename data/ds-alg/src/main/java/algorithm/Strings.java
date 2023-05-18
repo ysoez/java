b9c84c07a8a5b83ade9908cf1dfc1e817f7c0478
Dmitry <dmitry@blockearner.com.au>
2023-05-18 13:07:00 +0300

    [ds-alg] refactoring

--- data/ds-alg/src/main/java/algorithm/Strings.java
+++ data/ds-alg/src/main/java/algorithm/Strings.java
@@ -1,21 +1,28 @@
 package algorithm;
 
 import util.Algorithm;
+import util.Algorithm.Complexity;
 
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.LinkedHashMap;
 
 import static java.lang.Math.abs;
+import static java.util.Arrays.asList;
+import static java.util.Arrays.sort;
 import static util.Algorithm.Complexity.Value.CONSTANT;
 import static util.Algorithm.Complexity.Value.LINEAR;
+import static util.Algorithm.Complexity.Value.N_LOG_N;
+import static util.Algorithm.Complexity.Value.POLYNOMIAL;
 
-public class Strings {
+class Strings {
 
     static final String EMPTY = "";
     static final String SPACE = " ";
     static final int ASCII_SIZE = 256;
     static final int ENGLISH_ALPHABET = 26;
 
-    @Algorithm(complexity = @Algorithm.Complexity(runtime = LINEAR, space = LINEAR))
+    @Algorithm(complexity = @Complexity(runtime = LINEAR, space = LINEAR))
     static Character firstUniqueChar(String str) {
         var frequency = new LinkedHashMap<Character, Integer>();
         for (char c : str.toCharArray())
@@ -26,7 +33,33 @@ public class Strings {
         return null;
     }
 
-    @Algorithm(complexity = @Algorithm.Complexity(runtime = LINEAR, space = CONSTANT))
+    @Algorithm(complexity = @Complexity(runtime = LINEAR, space = LINEAR))
+    static Character firstRepeatedChar(String input) {
+        var charSet = new HashSet<Character>();
+        for (var ch : input.toCharArray())
+            if (!charSet.add(ch))
+                return ch;
+        return Character.MIN_VALUE;
+    }
+
+    @Algorithm(complexity = @Complexity(runtime = LINEAR, space = CONSTANT))
+    static char mostRepeatedChar(String str) {
+        if (str == null || str.isEmpty())
+            throw new IllegalArgumentException();
+        int[] frequencies = new int[ASCII_SIZE];
+        for (char ch : str.toCharArray())
+            frequencies[ch]++;
+        int max = 0;
+        char result = ' ';
+        for (int i = 0; i < frequencies.length; i++)
+            if (frequencies[i] > max) {
+                max = frequencies[i];
+                result = (char) i;
+            }
+        return result;
+    }
+
+    @Algorithm(complexity = @Complexity(runtime = LINEAR, space = CONSTANT))
     static boolean isOneAway(String first, String second) {
         if (first == null || second == null)
             return false;
@@ -58,18 +91,46 @@ public class Strings {
         return true;
     }
 
+    @Algorithm(complexity = @Complexity(runtime = LINEAR, space = CONSTANT))
+    static int vowelsCount(String str) {
+        if (str == null)
+            return 0;
+        var count = 0;
+        String vowels = "aeiou";
+        for (char ch : str.toLowerCase().toCharArray())
+            if (vowels.indexOf(ch) != -1)
+                count++;
+        return count;
+    }
 
+    @Algorithm(complexity = @Complexity(runtime = LINEAR, space = LINEAR))
+    static boolean areRotations(String str1, String str2) {
+        if (str1 == null || str2 == null)
+            return false;
+        return str1.length() == str2.length() && (str1 + str1).contains(str2);
+    }
 
-//    static int vowelsCount(String str) {
-//        if (str == null)
-//            return 0;
-//        var count = 0;
-//        String vowels = "aeiou";
-//        for (char ch : str.toLowerCase().toCharArray())
-//            if (vowels.indexOf(ch) != -1)
-//                count++;
-//        return count;
-//    }
+    @Algorithm(complexity = @Complexity(runtime = LINEAR, space = POLYNOMIAL))
+    static String removeDuplicates(String str) {
+        if (str == null)
+            return EMPTY;
+        var output = new StringBuilder();
+        var set = new HashSet<Character>();
+        for (char ch : str.toCharArray())
+            if (set.add(ch))
+                output.append(ch);
+        return output.toString();
+    }
+
+    @Algorithm(complexity = @Complexity(runtime = LINEAR, space = LINEAR))
+    static String capitalize(String sentence) {
+        if (sentence == null || sentence.trim().isEmpty())
+            return EMPTY;
+        String[] words = sentence.trim().replaceAll(" +", SPACE).split(SPACE);
+        for (int i = 0; i < words.length; i++)
+            words[i] = words[i].substring(0, 1).toUpperCase() + words[i].substring(1).toLowerCase();
+        return String.join(SPACE, words);
+    }
 
 //    static String reverseStack(String str) {
 //        var stack = new Stack<Character>();
@@ -81,116 +142,76 @@ public class Strings {
 //        return builder.toString();
 //    }
 
-//    static String reverse(String str) {
-//        if (str == null)
-//            return EMPTY;
-//        var reversed = new StringBuilder();
-//        char[] chars = str.toCharArray();
-//        for (int i = chars.length - 1; i >= 0; i--)
-//            reversed.append(chars[i]);
-//        return reversed.toString();
-//    }
+    @Algorithm(complexity = @Complexity(runtime = LINEAR, space = LINEAR))
+    static String reverse(String str) {
+        if (str == null)
+            return null;
+        var reversed = new StringBuilder();
+        char[] chars = str.toCharArray();
+        for (int i = chars.length - 1; i >= 0; i--)
+            reversed.append(chars[i]);
+        return reversed.toString();
+    }
 
-    //    static String reverseWords(String str) {
-//        if (str == null)
-//            return EMPTY;
-//        var reversed = new StringBuilder();
-//        String[] words = str.split(SPACE);
-//        for (int i = words.length - 1; i >= 0; i--)
-//            reversed.append(words[i]).append(SPACE);
-//        return reversed.toString().trim();
-//    }
+    @Algorithm(complexity = @Complexity(runtime = LINEAR, space = LINEAR))
+    static String reverseWords(String str) {
+        if (str == null)
+            return null;
+        var reversed = new StringBuilder();
+        String[] words = str.split(SPACE);
+        for (int i = words.length - 1; i >= 0; i--)
+            reversed.append(words[i]).append(SPACE);
+        return reversed.toString().trim();
+    }
 
-//    static String reverseWords(String sentence) {
-//        if (sentence == null)
-//            return EMPTY;
-//        String[] words = sentence.trim().split(SPACE);
-//        Collections.reverse(asList(words));
-//        return String.join(SPACE, words);
-//    }
-//
-//    static boolean areRotations(String str1, String str2) {
-//        if (str1 == null || str2 == null)
-//            return false;
-//        return str1.length() == str2.length() && (str1 + str1).contains(str2);
-//    }
-//
-//    static String removeDuplicates(String str) {
-//        if (str == null)
-//            return EMPTY;
-//        var output = new StringBuilder();
-//        var set = new HashSet<Character>();
-//        for (char ch : str.toCharArray())
-//            if (set.add(ch))
-//                output.append(ch);
-//        return output.toString();
-//    }
+    static String reverseSentence(String sentence) {
+        if (sentence == null)
+            return null;
+        String[] words = sentence.trim().split(SPACE);
+        Collections.reverse(asList(words));
+        return String.join(SPACE, words);
+    }
 
-//    static char maxOccuringChar(String str) {
-//
-//        Map<Character, Integer> frequencies = new HashMap<>();
-//        for (char ch : str.toCharArray()) {
-//            if (frequencies.containsKey(ch))
-//                frequencies.replace(ch, frequencies.get(ch) + 1);
-//            else
-//                frequencies.put(ch, 1);
-//        }
-//
-//    }
+    @Algorithm(complexity = @Complexity(runtime = N_LOG_N, space = CONSTANT))
+    static boolean areAnagrams(String str1, String str2) {
+        if (str1 == null || str2 == null || str1.length() != str2.length())
+            return false;
+        var array1 = str1.toLowerCase().toCharArray();
+        sort(array1);
+        var array2 = str2.toLowerCase().toCharArray();
+        sort(array2);
+        return java.util.Arrays.equals(array1, array2);
+    }
 
-//    static char maxOccuringChar(String str) {
-//        if (str == null || str.isEmpty())
-//            throw new IllegalArgumentException();
-//        int[] frequencies = new int[ASCII_SIZE];
-//        for (char ch : str.toCharArray())
-//            frequencies[ch]++;
-//        int max = 0;
-//        char result = ' ';
-//        for (int i = 0; i < frequencies.length; i++)
-//            if (frequencies[i] > max){
-//                max = frequencies[i];
-//                result = (char) i;
-//            }
-//        return result;
-//    }
-//
-//    static String capitalize(String sentence) {
-//        if (sentence == null || sentence.trim().isEmpty())
-//            return EMPTY;
-//        String[] words = sentence.trim().replaceAll(" +", SPACE).split(SPACE);
-//        for (int i = 0; i < words.length; i++)
-//            words[i] = words[i].substring(0, 1).toUpperCase() + words[i].substring(1).toLowerCase();
-//        return String.join(SPACE, words);
-//    }
-//
-//    // n logn
-//    static boolean areAnagrams(String str1, String str2) {
-//        if (str1 == null || str2 == null || str1.length() != str2.length())
-//            return false;
-//        var array1 = str1.toLowerCase().toCharArray();
-//        sort(array1);
-//        var array2 = str2.toLowerCase().toCharArray();
-//        sort(array2);
-//        return java.util.Arrays.equals(array1, array2);
-//    }
-//
-//    // o(n)
-//    static boolean areAnagramsHistogram(String str1, String str2) {
-//        if (str1 == null || str2 == null)
-//            return false;
-//        int[] frequencies = new int[ENGLISH_ALPHABET];
-//        str1 = str1.toLowerCase();
-//        for (int i = 0; i < str1.length(); i++)
-//            frequencies[str1.charAt(i) - 'a']++;
-//        str2 = str2.toLowerCase();
-//        for (int i = 0; i < str2.length(); i++) {
-//            int index = str2.charAt(i) - 'a';
-//            if (frequencies[index] == 0)
-//                return false;
-//            frequencies[index]--;
-//        }
-//        return true;
-//    }
+    @Algorithm(complexity = @Complexity(runtime = LINEAR, space = CONSTANT))
+    static boolean areAnagramsHistogram(String str1, String str2) {
+        if (str1 == null || str2 == null)
+            return false;
+        int[] frequencies = new int[ENGLISH_ALPHABET];
+        str1 = str1.toLowerCase();
+        for (int i = 0; i < str1.length(); i++)
+            frequencies[str1.charAt(i) - 'a']++;
+        str2 = str2.toLowerCase();
+        for (int i = 0; i < str2.length(); i++) {
+            int index = str2.charAt(i) - 'a';
+            if (frequencies[index] == 0)
+                return false;
+            frequencies[index]--;
+        }
+        return true;
+    }
+
+    @Algorithm(complexity = @Complexity(runtime = LINEAR, space = CONSTANT))
+    static boolean isPalindrome(String word) {
+        if (word == null)
+            return false;
+        int left = 0;
+        int right = word.length() - 1;
+        while (left < right)
+            if (word.charAt(left++) != word.charAt(right--))
+                return false;
+        return true;
+    }
 
     // 4 times iteration
 //    static boolean isPalindrome(String word) {
@@ -199,16 +220,25 @@ public class Strings {
 //        return reversed.toString().equals(word);
 //    }
 
-    // 0.5 iteration
-//    static boolean isPalindrome(String word) {
-//        if (word == null)
-//            return false;
-//        int left = 0;
-//        int right = word.length() - 1;
-//        while (left < right)
-//            if (word.charAt(left++) != word.charAt(right--))
-//                return false;
-//        return true;
-//    }
+
+    static int longestSubstringLength(String str) {
+        // aab
+        var longestSubstrLength = 0;
+        var uniqueChars = new HashSet<Character>();
+        for (int i = 0; i < str.length(); i++) {
+            String subStr = "";
+            boolean hasUniqueChars = false;
+            for (int j = i; j < str.length(); j++) {
+                char ch = str.charAt(j);
+                subStr += ch;
+                hasUniqueChars = uniqueChars.add(ch);
+            }
+            if (hasUniqueChars && subStr.length() > longestSubstrLength) {
+                longestSubstrLength = subStr.length();
+            }
+            uniqueChars.clear();
+        }
+        return longestSubstrLength;
+    }
 
 }
--- data/ds-alg/src/test/java/algorithm/StringsTest.java
+++ data/ds-alg/src/test/java/algorithm/StringsTest.java
@@ -2,6 +2,14 @@ package algorithm;
 
 import org.junit.jupiter.api.Test;
 
+import static algorithm.Strings.EMPTY;
+import static algorithm.Strings.SPACE;
+import static algorithm.Strings.areAnagrams;
+import static algorithm.Strings.areAnagramsHistogram;
+import static algorithm.Strings.areRotations;
+import static algorithm.Strings.capitalize;
+import static algorithm.Strings.isPalindrome;
+import static algorithm.Strings.reverseWords;
 import static org.junit.jupiter.api.Assertions.*;
 
 class StringsTest {
@@ -14,6 +22,13 @@ class StringsTest {
         assertEquals('c', Strings.firstUniqueChar("abcab"));
         assertEquals('c', Strings.firstUniqueChar("aabbbc"));
         assertEquals('d', Strings.firstUniqueChar("aabbdbc"));
+    }
+
+    @Test
+    void firstRepeatedChar() {
+        assertEquals('e', Strings.firstRepeatedChar("a green apple"));
+        assertEquals('e', Strings.firstRepeatedChar("geeksforgeeks"));
+        assertEquals(Character.MIN_VALUE, Strings.firstRepeatedChar("abcde"));
     }
 
     @Test
@@ -31,112 +46,123 @@ class StringsTest {
         assertFalse(Strings.isOneAway("abc", "bcc"));
     }
 
-//    @Test
-//    void countVowels() {
-//        assertEquals(0, vowelsCount(null));
-//        assertEquals(0, vowelsCount(EMPTY));
-//        assertEquals(0, vowelsCount("   "));
-//        assertEquals(3, vowelsCount("Hello World"));
-//        assertEquals(3, vowelsCount("HellO World"));
-//    }
-//
-//    @Test
-//    void reverseString() {
-//        assertEquals(EMPTY, reverse(null));
-//        assertEquals(EMPTY, reverse(EMPTY));
-//        assertEquals(SPACE, reverse(SPACE));
-//        assertEquals("der", reverse("red"));
-//        assertEquals("DER", reverse("RED"));
-//    }
-//
-//    @Test
-//    void reverseWordsInString() {
-//        assertEquals(EMPTY, reverseWords(null));
-//        assertEquals(EMPTY, reverseWords(EMPTY));
-//        assertEquals(EMPTY, reverseWords(SPACE));
-//        assertEquals(EMPTY, reverseWords("    "));
-//        assertEquals("Trees", reverseWords("Trees"));
-//        assertEquals("beautiful are Trees", reverseWords("Trees are beautiful"));
-//        assertEquals("beautiful are Trees", reverseWords("Trees are beautiful "));
-//        assertEquals("beautiful are Trees", reverseWords("  Trees are beautiful "));
-//    }
-//
-//    @Test
-//    void rotations() {
-//        assertFalse(areRotations("ABCD", null));
-//        assertFalse(areRotations("ABCD", SPACE));
-//        assertFalse(areRotations("ABCD", EMPTY));
-//        assertFalse(areRotations("ABCD", "DABC1"));
-//        assertFalse(areRotations("ABCD", "DACB"));
-//
-//        assertTrue(areRotations("ABCD", "DABC"));
-//        assertTrue(areRotations("ABCD", "CDAB"));
-//        assertTrue(areRotations("ABCD", "BCDA"));
-//        assertTrue(areRotations("ABCD", "ABCD"));
-//    }
-//
-//    @Test
-//    void removeDuplicateChars() {
-//        assertEquals(EMPTY, removeDuplicates(null));
-//        assertEquals(EMPTY, removeDuplicates(EMPTY));
-//        assertEquals(SPACE, removeDuplicates("  "));
-//        assertEquals("Tres abutifl", removeDuplicates("Trees are beautiful"));
-//    }
-//
-//    @Test
-//    void mostRepeatedChar() {
-//        assertThrows(IllegalArgumentException.class, () -> maxOccuringChar(null));
-//        assertThrows(IllegalArgumentException.class, () -> maxOccuringChar(EMPTY));
-//        assertEquals('e', maxOccuringChar("Trees are beautiful"));
-//    }
-//
-//    @Test
-//    void capitalizeEachWord() {
-//        assertEquals(EMPTY, capitalize(null));
-//        assertEquals(EMPTY, capitalize(EMPTY));
-//
-//        assertEquals(EMPTY, capitalize("   "));
-//        assertEquals("Today Is A Great Day", capitalize("    TODAY IS A GREAT DAY"));
-//        assertEquals("Today Is A Great Day", capitalize("    TODAY IS A GREAT DAY   "));
-//        assertEquals("Today Is A Great Day", capitalize("    TODAY   IS A    GREAT DAY   "));
-//
-//        assertEquals("Today Is A Great Day", capitalize("today is a great day"));
-//        assertEquals("Today Is A Great Day", capitalize("TODAY IS A GREAT DAY"));
-//    }
-//
-//    @Test
-//    void areAnagramStrings() {
-//        assertFalse(areAnagrams("ABCD", null));
-//        assertFalse(areAnagrams("ABCD", EMPTY));
-//        assertFalse(areAnagrams("ABCD", SPACE));
-//
-//        assertFalse(areAnagrams("ABCD1", "DBCA"));
-//        assertFalse(areAnagrams("ABCD", "DBCA1"));
-//        assertFalse(areAnagrams("ABCX", "DBCA"));
-//
-//        assertTrue(areAnagrams("ABCD", "DBCA"));
-//        assertTrue(areAnagrams("ABCd", "DBCA"));
-//
-////        assertFalse(areAnagramsHistogram("ABCD", null));
-////        assertFalse(areAnagramsHistogram("ABCD", EMPTY));
-////        assertFalse(areAnagramsHistogram("ABCD", SPACE));
-//
-////        assertFalse(areAnagramsHistogram("ABCD1", "DBCA"));
-////        assertFalse(areAnagramsHistogram("ABCD", "DBCA1"));
-////        assertFalse(areAnagramsHistogram("ABCX", "DBCA"));
-////
-////        assertTrue(areAnagramsHistogram("ABCD", "DBCA"));
-////        assertTrue(areAnagramsHistogram("ABCd", "DBCA"));
-//    }
-//
-//    @Test
-//    void isPalindromeString() {
-//        assertFalse(isPalindrome(null));
-//        assertFalse(isPalindrome("ABBC"));
-//        assertFalse(isPalindrome("ABBa"));
-//
-//        assertTrue(isPalindrome(EMPTY));
-//        assertTrue(isPalindrome("ABBA"));
-//    }
+    @Test
+    void countVowels() {
+        assertEquals(0, Strings.vowelsCount(null));
+        assertEquals(0, Strings.vowelsCount(EMPTY));
+        assertEquals(0, Strings.vowelsCount("   "));
+        assertEquals(3, Strings.vowelsCount("Hello World"));
+    }
 
-}
+    @Test
+    void rotations() {
+        assertFalse(areRotations("ABCD", null));
+        assertFalse(areRotations("ABCD", SPACE));
+        assertFalse(areRotations("ABCD", EMPTY));
+        assertFalse(areRotations("ABCD", "DABC1"));
+        assertFalse(areRotations("ABCD", "DACB"));
+
+        assertTrue(areRotations("ABCD", "DABC"));
+        assertTrue(areRotations("ABCD", "CDAB"));
+        assertTrue(areRotations("ABCD", "BCDA"));
+        assertTrue(areRotations("ABCD", "ABCD"));
+    }
+
+    @Test
+    void mostRepeatedChar() {
+        assertThrows(IllegalArgumentException.class, () -> Strings.mostRepeatedChar(null));
+        assertThrows(IllegalArgumentException.class, () -> Strings.mostRepeatedChar(EMPTY));
+        assertEquals('e', Strings.mostRepeatedChar("Trees are beautiful"));
+    }
+
+    @Test
+    void removeDuplicateChars() {
+        assertEquals(EMPTY, Strings.removeDuplicates(null));
+        assertEquals(EMPTY, Strings.removeDuplicates(EMPTY));
+        assertEquals(SPACE, Strings.removeDuplicates("  "));
+        assertEquals("Tres abutifl", Strings.removeDuplicates("Trees are beautiful"));
+    }
+
+    @Test
+    void capitalizeEachWord() {
+        assertEquals(EMPTY, capitalize(null));
+        assertEquals(EMPTY, capitalize(EMPTY));
+
+        assertEquals(EMPTY, capitalize("   "));
+        assertEquals("Today Is A Great Day", capitalize("    TODAY IS A GREAT DAY"));
+        assertEquals("Today Is A Great Day", capitalize("    TODAY IS A GREAT DAY   "));
+        assertEquals("Today Is A Great Day", capitalize("    TODAY   IS A    GREAT DAY   "));
+
+        assertEquals("Today Is A Great Day", capitalize("today is a great day"));
+        assertEquals("Today Is A Great Day", capitalize("TODAY IS A GREAT DAY"));
+    }
+
+    @Test
+    void reverse() {
+        assertNull(Strings.reverse((null)));
+        assertEquals(EMPTY, Strings.reverse(EMPTY));
+        assertEquals(SPACE, Strings.reverse(SPACE));
+        assertEquals("der", Strings.reverse("red"));
+        assertEquals("DER", Strings.reverse("RED"));
+    }
+
+    @Test
+    void reverseWordsInString() {
+        assertNull(reverseWords(null));
+        assertEquals(EMPTY, reverseWords(EMPTY));
+        assertEquals(EMPTY, reverseWords(SPACE));
+        assertEquals(EMPTY, reverseWords("    "));
+        assertEquals("Trees", reverseWords("Trees"));
+        assertEquals("beautiful are Trees", reverseWords("Trees are beautiful"));
+        assertEquals("beautiful are Trees", reverseWords("Trees are beautiful "));
+        assertEquals("beautiful are Trees", reverseWords("  Trees are beautiful "));
+    }
+
+    @Test
+    void areAnagramStrings() {
+        assertFalse(areAnagrams("ABCD", null));
+        assertFalse(areAnagrams("ABCD", EMPTY));
+        assertFalse(areAnagrams("ABCD", SPACE));
+
+        assertFalse(areAnagrams("ABCD1", "DBCA"));
+        assertFalse(areAnagrams("ABCD", "DBCA1"));
+        assertFalse(areAnagrams("ABCX", "DBCA"));
+
+        assertTrue(areAnagrams("ABCD", "DBCA"));
+        assertTrue(areAnagrams("ABCd", "DBCA"));
+
+        assertFalse(areAnagramsHistogram("ABCD", null));
+        assertFalse(areAnagramsHistogram("ABCD", EMPTY));
+        assertFalse(areAnagramsHistogram("ABCD", SPACE));
+
+        assertFalse(areAnagramsHistogram("ABCD1", "DBCA"));
+        assertFalse(areAnagramsHistogram("ABCD", "DBCA1"));
+        assertFalse(areAnagramsHistogram("ABCX", "DBCA"));
+
+        assertTrue(areAnagramsHistogram("ABCD", "DBCA"));
+        assertTrue(areAnagramsHistogram("ABCd", "DBCA"));
+    }
+
+    @Test
+    void isPalindromeString() {
+        assertFalse(isPalindrome(null));
+        assertFalse(isPalindrome("ABBC"));
+        assertFalse(isPalindrome("ABBa"));
+
+        assertTrue(isPalindrome(EMPTY));
+        assertTrue(isPalindrome("ABBA"));
+    }
+
+    @Test
+    void longestSubstringLength() {
+        assertEquals(1, Strings.longestSubstringLength(" "));
+        assertEquals(2, Strings.longestSubstringLength("au"));
+        assertEquals(2, Strings.longestSubstringLength("aab"));
+        assertEquals(3, Strings.longestSubstringLength("dvdf"));
+        assertEquals(3, Strings.longestSubstringLength("abcabcbb"));
+        assertEquals(1, Strings.longestSubstringLength("bbbbb"));
+        assertEquals(3, Strings.longestSubstringLength("pwwkew"));
+    }
+
+
+    }
