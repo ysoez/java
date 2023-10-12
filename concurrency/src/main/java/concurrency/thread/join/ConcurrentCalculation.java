ff5392c4600fa56bb3c3d16f1bc1c963e12ccc18
ysoez <dmitry.volontyr@gmail.com>
2023-10-12 22:32:16 +0300

    [concurrency] thread join

---
+++ concurrency/src/main/java/concurrency/thread/join/ConcurrentCalculation.java
@@ -0,0 +1,49 @@
+package concurrency.thread.join;
+
+import lombok.RequiredArgsConstructor;
+
+import java.math.BigInteger;
+
+class ConcurrentCalculation {
+
+    public static void main(String[] args) {
+        System.out.println(calculateResult(BigInteger.TEN, BigInteger.TWO, BigInteger.TWO, BigInteger.TEN));
+    }
+
+    public static BigInteger calculateResult(BigInteger base1,
+                                             BigInteger power1,
+                                             BigInteger base2,
+                                             BigInteger power2) {
+        BigInteger result;
+        PowerCalculatingThread thread1 = new PowerCalculatingThread(base1, power1);
+        PowerCalculatingThread thread2 = new PowerCalculatingThread(base2, power2);
+
+        thread1.start();
+        thread2.start();
+
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        result = thread1.result.add(thread2.result);
+        return result;
+    }
+
+    @RequiredArgsConstructor
+    private static class PowerCalculatingThread extends Thread {
+        private BigInteger result;
+        private final BigInteger base;
+        private final BigInteger power;
+
+        @Override
+        public void run() {
+            result = BigInteger.ONE;
+            for (BigInteger i = BigInteger.ZERO; i.compareTo(power) != 0; i = i.add(BigInteger.ONE)) {
+                result = result.multiply(base);
+            }
+        }
+    }
+}
