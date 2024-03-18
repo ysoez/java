e18b240fd7bf3ecc6a37e464259e1977c84c18e6
ysoez <dmitry.volontyr@gmail.com>
2024-03-18 20:55:56 +0100

    [collections] iterator remove for CopyOnWriteArrayList

---
+++ concurrency/src/main/java/concurrency/collection/CopyOnWriteArrayListIteratorRemove.java
@@ -0,0 +1,31 @@
+package concurrency.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+class CopyOnWriteArrayListIteratorRemove {
+
+    public static void main(String[] ags) throws InterruptedException {
+        var arrayList = new ArrayList<String>() {{
+            add("1");
+            add("2");
+            add("3");
+            add("4");
+        }};
+        iteratorRemoveLast(arrayList);
+        var copyOnWriteArrayList = new CopyOnWriteArrayList<>(List.of("1,", "2", "3", "4"));
+        iteratorRemoveLast(copyOnWriteArrayList);
+    }
+
+    private static void iteratorRemoveLast(Iterable<String> iterable) {
+        var iterator = iterable.iterator();
+        while (iterator.hasNext()) {
+            if (iterator.next().equals("4")) {
+                iterator.remove();
+            }
+        }
+        System.out.println(iterable);
+    }
+
+}
