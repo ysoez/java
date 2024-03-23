27a553aca14c9d4f836379dec0d22a8354a913d5
ysoez <dmitry.volontyr@gmail.com>
2024-03-23 13:01:54 +0100

    [netty] plain io server

---
+++ netty/src/main/java/netty/transport/PlainIoServer.java
@@ -0,0 +1,35 @@
+package netty.transport;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+
+@Slf4j
+class PlainIoServer {
+
+    public void serve(int port) {
+        try (final var socket = new ServerSocket(port)) {
+            while (true) {
+                final Socket clientSocket = socket.accept();
+                log.debug("Accepted connection from: {}", clientSocket);
+                System.out.println();
+                new Thread(() -> {
+                    OutputStream out;
+                    try (clientSocket) {
+                        out = clientSocket.getOutputStream();
+                        out.write("Hi!\r\n".getBytes(StandardCharsets.UTF_8));
+                        out.flush();
+                    } catch (IOException e) {
+                        log.error("Error happened: ", e);
+                    }
+                }).start();
+            }
+        } catch (IOException e) {
+            log.error("Error happened: ", e);
+        }
+    }
+}
