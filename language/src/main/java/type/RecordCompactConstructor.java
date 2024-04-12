fab0c77b094b1b5e12e2b685948c65bc81519f8c
ysoez <dmitry.volontyr@gmail.com>
2024-04-12 09:27:36 +0200

    [language] compact constructor

---
+++ language/src/main/java/type/RecordCompactConstructor.java
@@ -0,0 +1,20 @@
+package type;
+
+class RecordCompactConstructor {
+
+    public static void main(String[] args) {
+        System.out.println(new Year(25));
+    }
+
+    record Year(int year) {
+        Year {
+            if (year < 0) {
+                throw new IllegalArgumentException("negative number");
+            }
+            if (year < 100) {
+                year += 200;
+            }
+        }
+    }
+
+}
