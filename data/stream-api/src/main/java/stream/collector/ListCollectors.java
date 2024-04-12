2bd97961fb9759923e549aafe92283cb41dbb46d
ysoez <dmitry.volontyr@gmail.com>
2024-04-12 09:19:49 +0200

    [stream] list collectors

---
+++ data/stream-api/src/main/java/stream/collector/ListCollectors.java
@@ -0,0 +1,15 @@
+package stream.collector;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+class ListCollectors {
+
+    @SuppressWarnings("SimplifyStreamApiCallChains")
+    public static void main(String[] args) {
+        System.out.println(Stream.of(1, 2, 3).collect(Collectors.toList()).getClass());
+        System.out.println(Stream.of(1, 2, 3).collect(Collectors.toUnmodifiableList()).getClass());
+        System.out.println(Stream.of(1, 2, 3).toList().getClass());
+    }
+
+}
