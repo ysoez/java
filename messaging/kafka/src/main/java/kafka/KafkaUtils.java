a8a88f81458dd6113811a206c5aaed3e7ea0ac51
ysoez <dmitry.volontyr@gmail.com>
2024-05-12 10:50:06 +0200

    [kafka] simple consumer

--- messaging/kafka/src/main/java/kafka/KafkaUtils.java
+++ messaging/kafka/src/main/java/kafka/KafkaUtils.java
@@ -1,5 +1,8 @@
 package kafka;
 
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerConfig;
@@ -12,12 +15,24 @@ public class KafkaUtils {
     public static Producer<Long, String> newKafkaProducer(String bootstrapServers, String clientId,
                                                           Class<? extends Serializer<?>> key,
                                                           Class<? extends Serializer<?>> value) {
-        Properties properties = new Properties();
+        var properties = new Properties();
         properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         properties.put(ProducerConfig.CLIENT_ID_CONFIG, clientId);
         properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, key.getName());
         properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, value.getName());
         return new KafkaProducer<>(properties);
+    }
+
+    public static Consumer<Long, String> newKafkaConsumer(String bootstrapServers, String consumerGroup,
+                                                          Class<? extends Serializer<?>> key,
+                                                          Class<? extends Serializer<?>> value) {
+        var properties = new Properties();
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, key.getName());
+        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, value.getName());
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG, consumerGroup);
+        properties.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
+        return new KafkaConsumer<>(properties);
     }
 
 }
