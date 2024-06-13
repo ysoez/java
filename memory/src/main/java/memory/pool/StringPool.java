bde37dabdd4f40c667b6f9a45bef885ba20909d5
ysoez <dmitry.volontyr@gmail.com>
2024-06-13 20:39:58 +0200

    [memory] string pool

---
+++ memory/src/main/java/memory/pool/StringPool.java
@@ -0,0 +1,21 @@
+package memory.pool;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+class StringPool {
+
+    // ~ -XX:+PrintStringTableStatistics
+    // ~ -XX:StringTableSize=10785
+    public static void main(String[] args) {
+        var list = new ArrayList<String>();
+        var start = new Date();
+        for (int i = 0; i < 30_000_000; i++) {
+            String str = Integer.valueOf(i).toString().intern();
+            list.add(str);
+        }
+        var end = new Date();
+        System.out.printf("Elapsed time: %s ms\n\n", end.getTime() - start.getTime());
+    }
+
+}
