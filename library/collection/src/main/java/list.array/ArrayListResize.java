a1b773eaf0075dfb5012e725e2f05ba7337912c5
ysoez <dmitry.volontyr@gmail.com>
2024-06-22 14:56:56 +0200

    [collection] array list resize

---
+++ library/collection/src/main/java/list.array/ArrayListResize.java
@@ -0,0 +1,22 @@
+package list.array;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+class ArrayListResize {
+
+    public static void main(String[] args) {
+        int elementsCount = 100_000_000;
+        var start = new Date();
+        List<Element> list = new ArrayList<>(elementsCount);
+        for (int i = 0; i < elementsCount; i++) {
+            list.add(new Element(i, String.valueOf(i)));
+        }
+        var end = new Date();
+        System.out.printf("Execution time: %s ms", end.getTime() - start.getTime());
+    }
+
+    private record Element(int index, String value) {}
+
+}
