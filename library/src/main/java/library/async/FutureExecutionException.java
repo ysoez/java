50b0884cb5ead6fecc7dd834b2a031f8a55ff674
ysoez <dmitry.volontyr@gmail.com>
2024-11-10 13:19:16 +0100

    future execution exception

---
+++ library/src/main/java/library/async/FutureExecutionException.java
@@ -0,0 +1,17 @@
+package library.async;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+
+class FutureExecutionException {
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        try (var executor = Executors.newSingleThreadExecutor()) {
+            var future = executor.submit(() -> {
+                throw new IllegalStateException();
+            });
+            future.get();
+        }
+    }
+
+}
