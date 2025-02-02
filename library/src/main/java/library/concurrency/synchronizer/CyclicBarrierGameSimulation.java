7dc45d75c577ceef4202ca5cb98cf2245c0fc1e5
ysoez <dmitry.volontyr@gmail.com>
2025-02-02 12:46:30 +0100

    cyclic barrier

---
+++ library/src/main/java/library/concurrency/synchronizer/CyclicBarrierGameSimulation.java
@@ -0,0 +1,39 @@
+package library.concurrency.synchronizer;
+
+import lombok.RequiredArgsConstructor;
+
+import java.util.concurrent.CyclicBarrier;
+
+class CyclicBarrierGameSimulation {
+
+    public static void main(String[] args) {
+        var barrier = new CyclicBarrier(4, () -> System.out.println("Round started!"));
+        for (int i = 1; i <= 4; i++) {
+            new Player(barrier, "player-" + i).start();
+        }
+    }
+
+    @RequiredArgsConstructor
+    private static class Player extends Thread {
+
+        private final CyclicBarrier barrier;
+        private final String playerId;
+
+        @Override
+        public void run() {
+            try {
+                System.out.println(playerId + " is preparing...");
+                Thread.sleep((long) (Math.random() * 3000));
+                System.out.println(playerId + " is ready!");
+
+                barrier.await();
+
+                System.out.println(playerId + " is playing the round...");
+            } catch (Exception e) {
+                //noinspection CallToPrintStackTrace
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
