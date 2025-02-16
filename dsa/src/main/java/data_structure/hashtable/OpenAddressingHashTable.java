2d4fe39911868cdeb3c6e88edf46d227ccfd692b
ysoez <dmitry.volontyr@gmail.com>
2025-02-16 10:03:04 +0100

    refactor

--- dsa/src/main/java/data_structure/hashtable/OpenAddressingHashTable.java
+++ dsa/src/main/java/data_structure/hashtable/OpenAddressingHashTable.java
@@ -12,48 +12,53 @@ class OpenAddressingHashTable<K, V> implements HashTable<K, V> {
 
     @Override
     public void put(K key, V value) {
-        int attempt = 0;
-        int index = nextSlot(key, attempt);
-        while (entries[index] != null && !entries[index].isDeleted && entries[index].key != key)
-            index = nextSlot(key, ++attempt);
+        int index = findIndex(key, false);
         entries[index] = new Entry<>(key, value);
     }
 
     @Override
     public V get(K key) {
-        int attempt = 0;
-        int index = nextSlot(key, attempt);
-        int start = index;
-        while (entries[index] != null) {
-            if (!entries[index].isDeleted && entries[index].key == key) {
-                return entries[index].value;
-            }
-            attempt++;
-            index = nextSlot(key, attempt);
-            if (index == start)
-                break;
-        }
-        return null;
+        int index = findIndex(key, true);
+        return index != -1 ? entries[index].value : null;
     }
 
     @Override
     public void remove(K key) {
-        int attempt = 0;
-        int index = nextSlot(key, attempt);
-        int start = index;
-        while (entries[index] != null) {
-            if (!entries[index].isDeleted && entries[index].key == key) {
-                entries[index].isDeleted = true;
-                return;
-            }
-            attempt++;
-            index = nextSlot(key, attempt);
-            if (index == start)
-                break;
+        int index = findIndex(key, true);
+        if (index != -1) {
+            entries[index].isDeleted = true;
         }
     }
 
-    private int nextSlot(K key, int attempt) {
+    private int findIndex(K key, boolean searchOnly) {
+        int attempt = 0;
+        int index = nextIndex(key, attempt);
+        int start = index;
+        while (entries[index] != null) {
+            // ~ key found
+            if (!entries[index].isDeleted && entries[index].key.equals(key)) {
+                return index;
+            }
+            // ~ cycle detection
+            if (searchOnly && index == start) {
+                break;
+            }
+            // ~ probe next slot
+            attempt++;
+            index = nextIndex(key, attempt);
+        }
+        if (searchOnly) {
+            // ~ key not found
+            return -1;
+        }
+        //
+        // ~ the index of an existing key (for updating) or
+        // ~ the index of an empty or deleted slot (for insertion)
+        //
+        return index;
+    }
+
+    private int nextIndex(K key, int attempt) {
         return (hash(key) + probingStrategy.probe(attempt, tableSize())) % tableSize();
     }
 
@@ -67,7 +72,7 @@ class OpenAddressingHashTable<K, V> implements HashTable<K, V> {
 
     private static class Entry<K, V> implements HashTable.Entry<K, V> {
         private final K key;
-        private V value;
+        private final V value;
         private boolean isDeleted;
 
         Entry(K key, V value) {
@@ -107,6 +112,7 @@ class OpenAddressingHashTable<K, V> implements HashTable<K, V> {
         public int probe(int attempt, int tableSize) {
             return attempt * secondaryHash(attempt, tableSize) % tableSize;
         }
+
         private int secondaryHash(int key, int tableSize) {
             int prime = tableSize - 1;
             return prime - (key % prime);
