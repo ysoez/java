5efa434c23c530ed6ef029129f78d2d2a34f7fe3
ysoez <dmitry.volontyr@gmail.com>
2025-08-04 16:54:21 +0200

    static array

--- dsa/src/main/java/dsa/array/StaticArray.java
+++ dsa/src/main/java/dsa/array/StaticArray.java
@@ -5,41 +5,38 @@ import dsa.Algorithm.Complexity;
 
 import static dsa.Algorithm.Complexity.Value.CONSTANT;
 import static dsa.Algorithm.Complexity.Value.LINEAR;
-import static dsa.array.Arrays.*;
 
 class StaticArray<E> implements Array<E> {
 
-    private static final int DEFAULT_CAPACITY = 10;
-    protected E[] elements;
+    static final int DEFAULT_CAPACITY = 10;
+    private final E[] elements;
 
     @Algorithm(complexity = @Complexity(runtime = CONSTANT, space = CONSTANT))
     StaticArray() {
-        elements = newArray(DEFAULT_CAPACITY);
+        this(DEFAULT_CAPACITY);
     }
 
     @Algorithm(complexity = @Complexity(runtime = CONSTANT, space = LINEAR))
     StaticArray(int capacity) {
-        elements = newArray(capacity);
+        this.elements = Arrays.newArray(capacity);
     }
 
     @SafeVarargs
     @Algorithm(complexity = @Complexity(runtime = LINEAR, space = LINEAR))
     StaticArray(E... values) {
-        elements = newArray(values.length);
+        elements = Arrays.newArray(values.length);
         System.arraycopy(values, 0, elements, 0, values.length);
     }
 
     @Override
     @Algorithm(complexity = @Complexity(runtime = CONSTANT, space = CONSTANT))
     public void set(int index, E value) {
-        checkIndex(index, elements.length);
         elements[index] = value;
     }
 
     @Override
     @Algorithm(complexity = @Complexity(runtime = CONSTANT, space = CONSTANT))
     public E get(int index) {
-        checkIndex(index, elements.length);
         return elements[index];
     }
 
@@ -49,21 +46,4 @@ class StaticArray<E> implements Array<E> {
         return elements.length;
     }
 
-    @Override
-    @Algorithm(complexity = @Complexity(runtime = LINEAR, space = LINEAR))
-    public String toString() {
-        if (elements.length == 0) {
-            return "[]";
-        }
-        var builder = new StringBuilder("[");
-        for (int i = 0; i < elements.length; i++) {
-            builder.append(elements[i]);
-            if (i == elements.length - 1) {
-                builder.append("]");
-            } else {
-                builder.append(",");
-            }
-        }
-        return builder.toString();
-    }
 }
