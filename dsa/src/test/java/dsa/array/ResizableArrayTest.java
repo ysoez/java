fd5efb7d49381cbba87b10c45ea74c4a3149de6e
ysoez <dmitry.volontyr@gmail.com>
2025-08-20 09:20:56 +0200

    tests

--- dsa/src/test/java/dsa/array/ResizableArrayTest.java
+++ dsa/src/test/java/dsa/array/ResizableArrayTest.java
@@ -16,13 +16,30 @@ class ResizableArrayTest extends ListTest {
     @Test
     void testInsertLastAndGrow() {
         var array = new DynamicArray<>(3);
-
         array.insertLast(10);
         array.insertLast(20);
         array.insertLast(30);
         assertEquals(3, array.length());
 
         array.insertLast(40);
+
+        assertEquals(40, array.get(3));
+        assertEquals(4, array.size());
+        assertEquals(6, array.length());
+    }
+
+    @Test
+    void testInsertAtAndGrow() {
+        var array = new DynamicArray<>(3);
+        array.insertLast(10);
+        array.insertLast(20);
+        array.insertLast(30);
+        assertEquals(3, array.length());
+
+        array.insertAt(2, 25);
+
+        assertEquals(25, array.get(2));
+        assertEquals(4, array.size());
         assertEquals(6, array.length());
     }
 
