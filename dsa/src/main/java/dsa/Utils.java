2cd5f9d089736399a0561e0c4c5fe8c46f941986
ysoez <dmitry.volontyr@gmail.com>
2025-09-01 09:02:51 +0200

    refactor

--- dsa/src/main/java/dsa/Utils.java
+++ dsa/src/main/java/dsa/Utils.java
@@ -1,10 +1,12 @@
 package dsa;
 
+import dsa.Algorithm.Complexity;
+
 import static dsa.Algorithm.Complexity.CONSTANT;
 
 public class Utils {
 
-    @Algorithm(complexity = @Algorithm.Complexity(runtime = CONSTANT, space = CONSTANT))
+    @Algorithm(complexity = @Complexity(runtime = CONSTANT, space = CONSTANT))
     public static <E extends Comparable<E>> E min(E e1, E e2) {
         if (e1 == null)
             return e2;
@@ -13,13 +15,33 @@ public class Utils {
         return e1.compareTo(e2) <= 0 ? e1 : e2;
     }
 
-    @Algorithm(complexity = @Algorithm.Complexity(runtime = CONSTANT, space = CONSTANT))
+    @Algorithm(complexity = @Complexity(runtime = CONSTANT, space = CONSTANT))
     public static <E extends Comparable<E>> E max(E e1, E e2) {
         if (e1 == null)
             return e2;
         if (e2 == null)
             return e1;
         return e1.compareTo(e2) >= 0 ? e1 : e2;
+    }
+
+    @Algorithm(complexity = @Complexity(runtime = CONSTANT, space = CONSTANT))
+    public static <T extends Comparable<T>> boolean lessThan(T a, T b) {
+        return a.compareTo(b) < 0;
+    }
+
+    @Algorithm(complexity = @Complexity(runtime = CONSTANT, space = CONSTANT))
+    public static <T extends Comparable<T>> boolean lessThanOrEqual(T a, T b) {
+        return a.compareTo(b) <= 0;
+    }
+
+    @Algorithm(complexity = @Complexity(runtime = CONSTANT, space = CONSTANT))
+    public static <T extends Comparable<T>> boolean greaterThan(T a, T b) {
+        return a.compareTo(b) > 0;
+    }
+
+    @Algorithm(complexity = @Complexity(runtime = CONSTANT, space = CONSTANT))
+    public static <T extends Comparable<T>> boolean greaterThanOrEqual(T a, T b) {
+        return a.compareTo(b) >= 0;
     }
 
 }
