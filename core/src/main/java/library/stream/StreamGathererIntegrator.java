4b31ef3605245bcd48533874c9d8e2a3eb1b8d9d
ysoez <dmitry.volontyr@gmail.com>
2025-09-16 09:33:28 +0200

    stream stateless gatherer

---
+++ core/src/main/java/library/stream/StreamGathererIntegrator.java
@@ -0,0 +1,29 @@
+package library.stream;
+
+import java.util.stream.Gatherer;
+import java.util.stream.IntStream;
+
+class StreamGathererIntegrator {
+
+    public static void main(String[] args) {
+        IntStream.rangeClosed(1, 5)
+                .boxed()
+                .gather(Gatherer.of(new StatelessIntegrator()))
+                .forEach(System.out::println);
+    }
+
+    private static class StatelessIntegrator implements Gatherer.Integrator<Void, Integer, Integer> {
+        @Override
+        public boolean integrate(Void state, Integer element, Gatherer.Downstream<? super Integer> downstream) {
+            //
+            // ~ propagate doubled values to a downstream
+            //
+            downstream.push(element * 2);
+            //
+            // ~ consume all upstream
+            //
+            return true;
+        }
+    }
+
+}
