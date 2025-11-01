531cf56d92652d10254379ca453cc66f733ff7ea
ysoez <dmitry.volontyr@gmail.com>
2025-11-01 14:53:02 +0100

    refactor

--- check/distributed-systems/distributed-search/search-cluster-api/src/main/java/search/cluster/api/ServiceRegistryNamespace.java
+++
@@ -1,8 +0,0 @@
-package search.cluster.api;
-
-public class ServiceRegistryNamespace {
-
-    public static final String MASTER_SERVICE_REGISTRY = "/master";
-    public static final String WORKER_SERVICE_REGISTRY = "/worker";
-
-}
