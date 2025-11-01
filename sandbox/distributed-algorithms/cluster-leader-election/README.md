d4de8f704a9f3ab753b23512663ed2c93a4fac2d
ysoez <dmitry.volontyr@gmail.com>
2025-11-01 13:55:32 +0100

    leader election demo

---
+++ sandbox/distributed-algorithms/cluster-leader-election/README.md
@@ -0,0 +1,13 @@
+# Leader Election
+
+### Build Executable
+
+```
+./gradlew sandbox:distributed-algorithms:cluster-leader-election:build
+```
+
+### Run Application
+
+```
+java -jar sandbox/distributed-algorithms/cluster-leader-election/build/libs/leader-election-app-1.0.jar
+```
---
+++ sandbox/distributed-algorithms/cluster-leader-election/src/main/java/cluster/election/LeaderElectionRunner.java
@@ -0,0 +1,23 @@
+package cluster.election;
+
+import cluster.ClusterConnector;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.zookeeper.ZooKeeper;
+
+@Slf4j
+public class LeaderElectionRunner {
+
+    public static void main(String[] args) throws Exception {
+        try (var connector = new ClusterConnector()) {
+            ZooKeeper zooKeeper = connector.connect();
+            var leaderElection = new LeaderElection(zooKeeper);
+            //
+            // ~ initial leader election
+            //
+            leaderElection.electLeader();
+            connector.waitForDisconnect();
+        }
+        log.info("application exited");
+    }
+
+}
