16dd85bed0629ece9df8042ea2af5bfd7cb0e150
ysoez <dmitry.volontyr@gmail.com>
2026-01-10 11:41:15 +0100

    refactor

--- sandbox/dsa/src/main/java/dsa/array/StaticArray.java
+++ sandbox/dsa/src/main/java/dsa/array/StaticArray.java
@@ -8,24 +8,17 @@ import static dsa.Algorithm.Complexity.LINEAR;
 
 class StaticArray<E> implements Array<E> {
 
-    static final int DEFAULT_CAPACITY = 10;
+    private static final int DEFAULT_CAPACITY = 10;
     private final E[] elements;
 
-    @Algorithm(complexity = @Complexity(runtime = CONSTANT, space = CONSTANT))
-    StaticArray() {
-        this(DEFAULT_CAPACITY);
-    }
-
-    @Algorithm(complexity = @Complexity(runtime = CONSTANT, space = LINEAR))
+    @Algorithm(complexity = @Complexity(runtime = LINEAR, space = LINEAR))
     StaticArray(int capacity) {
         this.elements = Arrays.newArray(capacity);
     }
 
-    @SafeVarargs
-    @Algorithm(complexity = @Complexity(runtime = LINEAR, space = LINEAR))
-    StaticArray(E... values) {
-        elements = Arrays.newArray(values.length);
-        System.arraycopy(values, 0, elements, 0, values.length);
+    @Algorithm(complexity = @Complexity(runtime = CONSTANT, space = CONSTANT))
+    StaticArray() {
+        this(DEFAULT_CAPACITY);
     }
 
     @Override
