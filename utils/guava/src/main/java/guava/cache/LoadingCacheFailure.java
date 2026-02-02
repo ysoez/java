38f806c6a0e80edfb055898d87588ff8f362250e
ysoez <dmitry.volontyr@gmail.com>
2026-02-02 09:15:23 +0100

    guava samples

---
+++ utils/guava/src/main/java/guava/cache/LoadingCacheFailure.java
@@ -0,0 +1,43 @@
+package guava.cache;
+
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.MoreExecutors;
+
+import java.util.concurrent.Executors;
+
+public class LoadingCacheFailure {
+
+    public static void main(String[] args) {
+        try (var loaderPool = MoreExecutors.listeningDecorator(Executors.newSingleThreadExecutor())) {
+            LoadingCache<String, ListenableFuture<String>> cache = CacheBuilder.newBuilder().build(new CacheLoader<>() {
+                @Override
+                public ListenableFuture<String> load(String key) {
+                    return loaderPool.submit(() -> {
+                        if ("bad".equals(key)) {
+                            throw new RuntimeException("temporary failure");
+                        }
+                        return "value-" + key;
+                    });
+                }
+            });
+            ListenableFuture<String> future = cache.getUnchecked("bad");
+            Futures.addCallback(future, new FutureCallback<>() {
+                        public void onSuccess(String v) {
+                        }
+                        public void onFailure(Throwable t) {
+                            System.out.println("Load failed, invalidating cache");
+                            cache.invalidate("bad");
+                        }
+                    },
+                    MoreExecutors.directExecutor()
+            );
+            loaderPool.shutdown();
+        }
+    }
+
+}
