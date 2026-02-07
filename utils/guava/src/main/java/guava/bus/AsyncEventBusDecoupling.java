3c24550436fa4fcb709a454ecea2a105f433b792
ysoez <dmitry.volontyr@gmail.com>
2026-02-07 18:26:46 +0100

    guava event bus

---
+++ utils/guava/src/main/java/guava/bus/AsyncEventBusDecoupling.java
@@ -0,0 +1,33 @@
+package guava.bus;
+
+import com.google.common.eventbus.AsyncEventBus;
+import com.google.common.eventbus.Subscribe;
+
+import java.util.concurrent.Executors;
+import java.util.function.Consumer;
+
+class AsyncEventBusDecoupling {
+
+    public static void main(String[] args) {
+        try (var executor = Executors.newFixedThreadPool(2)) {
+            var eventBus = new AsyncEventBus(executor);
+            eventBus.register(new Consumer<String>() {
+                @Override
+                @Subscribe
+                public void accept(String event) {
+                    System.out.println("sending email to " + event);
+                }
+            });
+            eventBus.register(new Consumer<String>() {
+                @Override
+                @Subscribe
+                public void accept(String event) {
+                    System.out.println("auditing: " + event);
+                }
+            });
+            eventBus.post("user");
+        }
+        System.out.println("application exited");
+    }
+
+}
---
+++ utils/guava/src/main/java/guava/bus/EventBusDecoupling.java
@@ -0,0 +1,30 @@
+package guava.bus;
+
+import com.google.common.eventbus.EventBus;
+import com.google.common.eventbus.Subscribe;
+
+import java.util.function.Consumer;
+
+class EventBusDecoupling {
+
+    public static void main(String[] args) {
+        var eventBus = new EventBus();
+        eventBus.register(new Consumer<String>() {
+            @Override
+            @Subscribe
+            public void accept(String event) {
+                System.out.println("sending email to " + event);
+            }
+        });
+        eventBus.register(new Consumer<String>() {
+            @Override
+            @Subscribe
+            public void accept(String event) {
+                System.out.println("auditing: " + event);
+            }
+        });
+        eventBus.post("user");
+        System.out.println("application exited");
+    }
+
+}
