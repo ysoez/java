56aaf6dc510a92cae0f594b5f88bb5a53726b9bb
ysoez <dmitry.volontyr@gmail.com>
2026-02-08 12:36:40 +0100

    add equals/hashcode to node class

--- sandbox/dsa/src/main/java/dsa/Node.java
+++ sandbox/dsa/src/main/java/dsa/Node.java
@@ -1,13 +1,31 @@
 package dsa;
 
+import java.util.Objects;
+
 public class Node<E> {
 
     public E value;
     public Node<E> next;
 
+    public Node(E value) {
+        this.value = value;
+    }
+
     public Node(E value, Node<E> next) {
         this.value = value;
         this.next = next;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Node<?> node = (Node<?>) o;
+        return Objects.equals(value, node.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(value);
     }
 
 }
